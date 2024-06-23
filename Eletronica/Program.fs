6daea90bbfde7539d57558804ef8ce875aee57fd
Ygor Vieira <ygorus@gmail.com>
2024-06-23 11:24:49 -0300

    Teoremas de Thevenin e Norton

---
+++ Eletronica/Program.fs
@@ -0,0 +1,23 @@
+﻿// Definindo os valores do circuito
+let Vs = 12.0 // Fonte de tensão em Volts
+let R1 = 4.0 // Resistor R1 em Ohms
+let R2 = 6.0 // Resistor R2 em Ohms
+let RL = 8.0 // Resistor de carga em Ohms
+
+// Corrente de Norton (IN)
+let IN = Vs / (R1 + R2)
+
+// Resistência de Norton (RN)
+let RN = (R1 * R2) / (R1 + R2)
+
+// Corrente no resistor de carga (IL) utilizando o circuito equivalente de Norton
+let IL = IN * (RN / (RN + RL))
+
+// Tensão no resistor de carga (VL) utilizando a lei de Ohm
+let VL = IL * RL
+
+// Resultados
+printfn "Corrente de Norton (IN): %f A" IN
+printfn "Resistência de Norton (RN): %f Ω" RN
+printfn "Corrente no resistor de carga (IL): %f A" IL
+printfn "Tensão no resistor de carga (VL): %f V" VL
---
+++ Eletronica/Thevenin.fs
@@ -0,0 +1,21 @@
+﻿module Thevenin
+
+// Definindo os valores do circuito
+let Vs = 12.0 // Fonte de tensão em Volts
+let R1 = 4.0 // Resistor R1 em Ohms
+let R2 = 6.0 // Resistor R2 em Ohms
+let RL = 8.0 // Resistor de carga em Ohms
+
+// Tensão de Thevenin (Vth)
+let Vth = Vs * (R2 / (R1 + R2))
+
+// Resistência de Thevenin (Rth)
+let Rth = (R1 * R2) / (R1 + R2)
+
+// Tensão no resistor de carga (VL) utilizando o circuito equivalente de Thevenin
+let VL = Vth * (RL / (Rth + RL))
+
+// Resultados
+printfn "Tensão de Thevenin (Vth): %f V" Vth
+printfn "Resistência de Thevenin (Rth): %f Ω" Rth
+printfn "Tensão no resistor de carga (VL): %f V" VL
